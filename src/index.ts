b4adb70dccba38c75772e1a1f1376383a2e3d50f
Nikita Sergeev <sergeev.nickitos@yandex.ru>
2020-08-19 14:22:34 +0300

    SC-242 common: added exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { MessageAttachment } from './app/common/entities/message-attachment/message-attachment';
 export { AppMessage } from './app/common/types/imap/app-message';
 export { Criteria } from './app/common/types/imap/imap-data';
 export { ImapClient } from './app/imap-client';
