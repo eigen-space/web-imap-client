9de3d090cc85396d504b32dafbc9d64b7f1ca147
Nikita Agupov <nktrsk@gmail.com>
2020-10-06 20:31:55 +0300

    #3 imap-client: added a possibility to make disconnect and reconnect, added custom types and removed some ts-ignore

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export { SendMessageOptions } from './app/common/types/imap/send-message-options';
-export { UID } from './app/common/types/imap/imap-data';
+export { SendMessageOptions } from './app/common/types/send-message-options';
+export { UID } from './app/common/types/imap-data';
 export { MessageAttachment } from './app/common/entities/message-attachment/message-attachment';
-export { AppMessage } from './app/common/types/imap/app-message';
-export { Criteria } from './app/common/types/imap/imap-data';
+export { AppMessage } from './app/common/types/app-message';
+export { Criteria } from './app/common/types/imap-data';
 export { ImapClient } from './app/imap-client';
